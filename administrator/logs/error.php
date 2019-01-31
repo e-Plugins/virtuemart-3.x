cf303e98c080bcf8701398a3d889f6cb255cf1cf
petergrr01 <peter@targetmedia.eu>
2019-01-31 14:29:37 +0100

    new zip version

--- administrator/logs/error.php
+++
@@ -1,9 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2016-11-21 08:32:54 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2016-11-21T08:32:54+00:00	INFO 127.0.0.1	cookiefailure	Empty password not allowed.
-2016-11-21T11:03:23+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
-2016-11-21T11:23:22+00:00	INFO 127.0.0.1	joomlafailure	Empty password not allowed.
